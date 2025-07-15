8c3334c3a5e37c2f048743033a86f567f7b68eed
HaseebImd <haseeb.imdad@yahoo.com>
2025-07-15 15:05:40 +0500

    refactor routing structure and update Navbar links to use NavLink

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import { Route, Routes } from 'react-router'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes } from 'react-router'
 import Home from './pages/Home'
 import About from './pages/About'
 import Product from './pages/Product'
 import Contact from './pages/Contact'
+import RootLayout from './layout/RootLayout'
 
 const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="products" element={<Product />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
+    )
+  )
   return (
-    <div>
-      <Navbar />
-      <div className='container'>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/products" element={<Product />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import logo from '../assets/logo.svg'
-import { Link } from 'react-router'
+import { Link, NavLink } from 'react-router'
 const Navbar = () => {
     return (
         <div className="navbar">
             <img src={logo} style={{ height: "100px", width: "100px" }} alt="Logo" />
             <ul>
-                <Link to="/"><li>Home</li></Link>
-                <Link to="/products"><li>Products</li></Link>
-                <Link to="/about"><li>About</li></Link>
-                <Link to="/contact"><li>Contact</li></Link>
+                <NavLink to="/"><li>Home</li></NavLink>
+                <NavLink to="/products"><li>Products</li></NavLink>
+                <NavLink to="/about"><li>About</li></NavLink>
+                <NavLink to="/contact"><li>Contact</li></NavLink>
             </ul>
             <button>Get Started</button>
         </div>
